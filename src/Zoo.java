19fc9653b9976b2020abaa54178f09938e9b248a
Ricardo Weiss <ricardohsweiss@gmail.com>
2018-05-30 16:15:41 -0300

    created new instance of Animal, and edited the dog instance

--- src/Zoo.java
+++ src/Zoo.java
@@ -2,12 +2,13 @@
 public class Zoo {
 	public static void main(String args[]) {
 		
-		Animal zebra = new Animal("Zebra", 10, "large");
-		Animal dog = new Animal("Dog", 6, "medium");
+		Animal zebra = new Animal("Zebra", 10, "medium");
+		Animal lion = new Animal("Lion", 6, "medium");
+		Animal elephant = new Animal("Elephant", 25, "large");
 		
 		zebra.eat();
-		dog.move();
-		
+		lion.move();
+		elephant.sleep();
 		
 	}
 }

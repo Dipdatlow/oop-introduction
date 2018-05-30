b045ce046ce6c4368628446944672d7ed9bc2953
Ricardo Weiss <ricardohsweiss@gmail.com>
2018-05-30 11:09:57 -0300

    Adding 2 example classes

---
+++ src/Earth.java
@@ -0,0 +1,13 @@
+
+public class Earth {
+	public static void main(String Args[]) {
+		
+		Human human1 = new Human("Ricardo", 20, 70, "brown");
+		Human human2 = new Human("Rafael", 25, 60, "black");
+		Human human3 = new Human("Fatima", 55, 50, "blue");
+		
+		human1.speak();
+		human2.speak();
+		human3.speak();
+	}
+}

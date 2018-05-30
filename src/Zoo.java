fc4c1b144057a13456b79a56ddafbf17e1d42d7f
Ricardo Weiss <ricardohsweiss@gmail.com>
2018-05-30 16:08:42 -0300

    Added the Animal class, and the Zoo class

---
+++ src/Zoo.java
@@ -0,0 +1,13 @@
+
+public class Zoo {
+	public static void main(String args[]) {
+		
+		Animal zebra = new Animal("Zebra", 10, "large");
+		Animal dog = new Animal("Dog", 6, "medium");
+		
+		zebra.eat();
+		dog.move();
+		
+		
+	}
+}
